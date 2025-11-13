6394f1deccb968fa33e2045c40d786cfa6fc7a09
Sandoval Mija Carlos Manuel <sagitario_d55@hotmail.com>
2025-11-12 21:58:06 -0500

    11

--- models/cnn_basica_def.py
+++ models/cnn_basica_def.py
@@ -1,29 +1,24 @@
 import torch
 import torch.nn as nn
-from collections import OrderedDict
+import torch.nn.functional as F
 
 class CNNSimple(nn.Module):
     def __init__(self, num_classes=5):
         super().__init__()
-        # Definir con nombres explícitos iguales a los del checkpoint
-        self.f = nn.Sequential(OrderedDict([
-            ("0", nn.Conv2d(3, 32, 3, padding=1)),
-            ("1", nn.ReLU()),
-            ("3", nn.Conv2d(32, 64, 3, padding=1)),
-            ("4", nn.ReLU()),
-            ("6", nn.Conv2d(64, 128, 3, padding=1)),
-            ("7", nn.ReLU()),
-            ("9", nn.Conv2d(128, 256, 3, padding=1)),
-            ("10", nn.ReLU()),
-            ("11", nn.AdaptiveAvgPool2d((1, 1))),
-            ("12", nn.Flatten())
-        ]))
-
-        self.c = nn.Sequential(OrderedDict([
-            ("3", nn.Linear(256, num_classes))
-        ]))
+        # capas con los nombres exactos del checkpoint
+        self.f0 = nn.Conv2d(3, 32, 3, padding=1)
+        self.f3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.f6 = nn.Conv2d(64, 128, 3, padding=1)
+        self.f9 = nn.Conv2d(128, 256, 3, padding=1)
+        self.pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.c3 = nn.Linear(256, num_classes)
 
     def forward(self, x):
-        x = self.f(x)
-        x = self.c(x)
+        x = F.relu(self.f0(x))
+        x = F.relu(self.f3(x))
+        x = F.relu(self.f6(x))
+        x = F.relu(self.f9(x))
+        x = self.pool(x)
+        x = torch.flatten(x, 1)
+        x = self.c3(x)
         return x
