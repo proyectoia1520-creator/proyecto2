19c9cede134f84e0c623847e5ccbcdb3664a470c
Sandoval Mija Carlos Manuel <sagitario_d55@hotmail.com>
2025-11-12 21:56:35 -0500

    10

--- models/cnn_basica_def.py
+++ models/cnn_basica_def.py
@@ -1,38 +1,27 @@
-# models/cnn_basica_def.py
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from collections import OrderedDict
 
 class CNNSimple(nn.Module):
     def __init__(self, num_classes=5):
         super().__init__()
-        self.f = nn.Sequential(
-            # f.0
-            nn.Conv2d(3, 32, 3, padding=1),
-            # f.1
-            nn.ReLU(),
-            # f.2
-            nn.Conv2d(32, 64, 3, padding=1),
-            # f.3
-            nn.ReLU(),
-            # f.4
-            nn.Conv2d(64, 128, 3, padding=1),
-            # f.5
-            nn.ReLU(),
-            # f.6
-            nn.Conv2d(128, 256, 3, padding=1),
-            # f.7
-            nn.ReLU(),
-            # f.8
-            nn.AdaptiveAvgPool2d((1, 1)),
-            # f.9
-            nn.Flatten()
-        )
+        # Definir con nombres explícitos iguales a los del checkpoint
+        self.f = nn.Sequential(OrderedDict([
+            ("0", nn.Conv2d(3, 32, 3, padding=1)),
+            ("1", nn.ReLU()),
+            ("3", nn.Conv2d(32, 64, 3, padding=1)),
+            ("4", nn.ReLU()),
+            ("6", nn.Conv2d(64, 128, 3, padding=1)),
+            ("7", nn.ReLU()),
+            ("9", nn.Conv2d(128, 256, 3, padding=1)),
+            ("10", nn.ReLU()),
+            ("11", nn.AdaptiveAvgPool2d((1, 1))),
+            ("12", nn.Flatten())
+        ]))
 
-        # c.0
-        self.c = nn.Sequential(
-            nn.Linear(256, num_classes)
-        )
+        self.c = nn.Sequential(OrderedDict([
+            ("3", nn.Linear(256, num_classes))
+        ]))
 
     def forward(self, x):
         x = self.f(x)
