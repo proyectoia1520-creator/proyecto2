cecd0b038ca298af6bab0bf59faeb6cad6efca61
Sandoval Mija Carlos Manuel <sagitario_d55@hotmail.com>
2025-11-12 21:55:28 -0500

    9

--- models/cnn_basica_def.py
+++ models/cnn_basica_def.py
@@ -1,3 +1,4 @@
+# models/cnn_basica_def.py
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -5,21 +6,30 @@ import torch.nn.functional as F
 class CNNSimple(nn.Module):
     def __init__(self, num_classes=5):
         super().__init__()
-        # Bloque de características: coincide con f.0, f.3, f.6, f.9
         self.f = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, padding=1),   # f.0
-            nn.ReLU(),                                   # f.1
-            nn.Conv2d(32, 64, kernel_size=3, padding=1), # f.3
-            nn.ReLU(),                                   # f.4
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),# f.6
-            nn.ReLU(),                                   # f.7
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),# f.9
-            nn.ReLU(),                                   # f.10
+            # f.0
+            nn.Conv2d(3, 32, 3, padding=1),
+            # f.1
+            nn.ReLU(),
+            # f.2
+            nn.Conv2d(32, 64, 3, padding=1),
+            # f.3
+            nn.ReLU(),
+            # f.4
+            nn.Conv2d(64, 128, 3, padding=1),
+            # f.5
+            nn.ReLU(),
+            # f.6
+            nn.Conv2d(128, 256, 3, padding=1),
+            # f.7
+            nn.ReLU(),
+            # f.8
             nn.AdaptiveAvgPool2d((1, 1)),
+            # f.9
             nn.Flatten()
         )
 
-        # Bloque clasificador: c.3.weight, c.3.bias
+        # c.0
         self.c = nn.Sequential(
             nn.Linear(256, num_classes)
         )
