5c6fa7e26380ccee5be65e0c3ea485edd4d468ec
Sandoval Mija Carlos Manuel <sagitario_d55@hotmail.com>
2025-11-12 21:51:26 -0500

    6

--- models/cnn_basica_def.py
+++ models/cnn_basica_def.py
@@ -5,18 +5,15 @@ import torch.nn.functional as F
 class CNNSimple(nn.Module):
     def __init__(self, num_classes=5):
         super().__init__()
-        # bloque de features (según tus pesos)
         self.f = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, padding=1), nn.ReLU(),
             nn.Conv2d(32, 64, kernel_size=3, padding=1), nn.ReLU(),
             nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.ReLU(),
-            nn.Conv2d(128, 256, kernel_size=3, padding=1), nn.ReLU(),
             nn.AdaptiveAvgPool2d((1, 1)),
             nn.Flatten()
         )
-        # bloque de clasificación
         self.c = nn.Sequential(
-            nn.Linear(256, num_classes)
+            nn.Linear(128, num_classes)
         )
 
     def forward(self, x):
